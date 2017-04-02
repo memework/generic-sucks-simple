89838417b77edc5dfbd05d3fddf800b08e2aaa92
Motion <21dmurphy@cr.k12.ia.us>
2017-04-02 18:11:07 -0500

    Just a lil cleanup, don't merge if it doesn't work

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
+let express = require('express');
+let path = require('path');
+let serveStatic = require('serve-static');
  
-var app = express()
+let app = express();
 
 // HTTPS redir code - originally (c) 2014 IBM Corp. 
 // Modified redir code - (c) 2017 motiontheking
 app.enable('trust proxy');
 app.use (function (req, res, next) {
-        if (req.secure) {
-                // request was via https, so do no special handling
-                next();
-        } else {
-                // request was via http, so redirect to https
-                res.redirect(`https://${req.headers.host}${req.url}`);
-        }
+        if (req.secure)
+              next(); // request was via https, so do no special handling
+        else        
+              res.redirect(`https://${req.headers.host}${req.url}`); // request was via http, so redirect to https
 });
 
-app.use(serveStatic(path.join(__dirname, 'views')))
-app.listen(3000)
+app.use(serveStatic(path.join(__dirname, 'views')));
+app.listen(3000);
